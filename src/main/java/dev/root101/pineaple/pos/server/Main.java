cb645220551073cf46a91594d48aa0e27cba3182
JHB <jhernandezb96@gmail.com>
2022-03-24 02:20:53 -0400

    Spring dependencies updated 24-3-2022

--- src/main/java/dev/root101/pineaple/pos/server/Main.java
+++ src/main/java/dev/root101/pineaple/pos/server/Main.java
@@ -1,8 +1,30 @@
 package dev.root101.pineaple.pos.server;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value = "/admin")
+public class Main extends SpringBootServletInitializer {
+
+    private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        context = SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
+    }
+
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hello World!!! From the server side";
     }
 }
