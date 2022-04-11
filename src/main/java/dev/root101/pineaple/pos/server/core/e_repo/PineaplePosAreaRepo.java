a08abdd262a8ba52656963215858e22167005ba0
JHB <jhernandezb96@gmail.com>
2022-04-11 00:08:10 -0400

    Created Files with Camel Case

---
+++ src/main/java/dev/root101/pineaple/pos/server/core/e_repo/PineaplePosAreaRepo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.root101.pineaple.pos.server.core.e_repo;
+
+import dev.root101.clean.core.repo.CRUDRepository;
+import dev.root101.pineaple.pos.server.core.b_domain.PineaplePosDomains;
+
+/**
+ *
+ * @author JesusHdezWaterloo@Github
+ */
+public interface PineaplePosAreaRepo extends CRUDRepository<PineaplePosDomains.PineaplePOSAreaDomain, Integer> {
+
+}
